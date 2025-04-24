8cec7fef2b4f59e707c9d5027ea69f7fc0c50e9c
Habtsh-21 <hworku901@gmail.com>
2025-04-24 22:00:23 +0300

    configure firebase in the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:blockchain_based_national_election_admin_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
